b15ec16956f16144a497db5a0baf4b967e972a31
Sanid-Assara <sanid.assara@gmail.com>
2024-06-25 23:45:48 +0200

    Display Cards from an Array of Objects.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,27 @@
 const Cards = () => {
+  const localStorageData = [
+    { id: 1, title: "title A", date: "2024-06-25" },
+    { id: 2, title: "title B", date: "2024-06-24" },
+    { id: 3, title: "title C", date: "2024-06-23" },
+    { id: 4, title: "title Z", date: "2024-06-20" },
+  ];
+
+  localStorage.setItem("dairyCards", JSON.stringify(localStorageData));
+
   return (
-    <main className="p-8">
-      <div className="flex flex-col gap-1 max-w-52">
-        <figure>
-          <img src="https://picsum.photos/200" alt="" />
-        </figure>
-        <h1>Title</h1>
-        <time datetime="2024-06-25">June 25, 2024</time>
-        <section>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </section>
-      </div>
+    <main className="grid grid-cols-4 place-items-center p-8">
+      {localStorageData.map((card) => (
+        <div className="flex flex-col gap-1 max-w-52">
+          <figure>
+            <img src="https://picsum.photos/200" alt="" />
+          </figure>
+          <h1>{localStorageData.title}</h1>
+          <time>{localStorageData.date}</time>
+          <section>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </section>
+        </div>
+      ))}
     </main>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer class="bg-slate-900 text-white flex items-center justify-center gap-6 mx-auto py-8 px-4">
-      <small class="font-semibold sm:text-lg md:text-xl ">
+    <footer className="bg-slate-900 text-white flex items-center justify-center gap-6 mx-auto py-8 px-4">
+      <small className="font-semibold sm:text-lg md:text-xl ">
         &copy; 2024 Personal Diary. All rights reserved.
       </small>
     </footer>
