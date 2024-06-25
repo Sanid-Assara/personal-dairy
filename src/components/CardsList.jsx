9bc33d072e5348da5346cf56d5388875756142f6
Sanid-Assara <sanid.assara@gmail.com>
2024-06-26 00:30:45 +0200

    Add Card component and CardsList component.

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,4 +1,5 @@
-const Cards = () => {
+import Card from "./Card";
+const CardsList = () => {
   const localStorageData = [
     { id: 1, title: "title A", date: "2024-06-25" },
     { id: 2, title: "title B", date: "2024-06-24" },
@@ -6,24 +7,17 @@ const Cards = () => {
     { id: 4, title: "title Z", date: "2024-06-20" },
   ];
 
+  console.log(localStorageData[0].id);
+
   localStorage.setItem("dairyCards", JSON.stringify(localStorageData));
 
   return (
     <main className="grid grid-cols-4 place-items-center p-8">
       {localStorageData.map((card) => (
-        <div className="flex flex-col gap-1 max-w-52">
-          <figure>
-            <img src="https://picsum.photos/200" alt="" />
-          </figure>
-          <h1>{localStorageData.title}</h1>
-          <time>{localStorageData.date}</time>
-          <section>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          </section>
-        </div>
+        <Card key={card.id} title={card.title} date={card.date} />
       ))}
     </main>
   );
 };
 
-export default Cards;
+export default CardsList;
