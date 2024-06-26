cd7ba14b43b3c3fb77d9e1815fa921036a4a3388
Sanid-Assara <sanid.assara@gmail.com>
2024-06-27 00:42:08 +0200

    Rename AddEntryModal.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import AddEntry from "./AddEntry";
+import AddEntryModal from "./AddEntryModal";
 const Header = () => {
   return (
     <>
       <header className="bg-slate-900 flex items-center justify-between  mx-auto py-8 px-4">
         <p className="text-white">Personal Dairy</p>
-        <AddEntry />
+        <AddEntryModal />
       </header>
     </>
   );
