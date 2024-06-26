85f487ae2d425edc7fa174f27e9c92aa73689516
Sanid-Assara <sanid.assara@gmail.com>
2024-06-27 01:37:31 +0200

    Make footer fixed in the bottom.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-slate-900 text-white flex items-center justify-center gap-6 mx-auto py-8 px-4">
+    <footer className="bg-slate-900 text-white flex items-center justify-center gap-6 mx-auto py-8 px-4 fixed bottom-0 w-full">
       <small className="font-semibold sm:text-lg md:text-xl ">
         &copy; 2024 Personal Diary. All rights reserved.
       </small>
