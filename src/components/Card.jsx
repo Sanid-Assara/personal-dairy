b42aeb749be7b4decdaf6b0179e62bb59ad47b89
Sanid-Assara <sanid.assara@gmail.com>
2024-06-26 16:46:30 +0200

    Add Sort by date feature.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 const Card = ({ id, title, date, url }) => {
   return (
-    <div className="flex flex-col gap-1 max-w-52">
+    <div className="flex flex-col gap-1 max-w-52 mb-4">
       <figure>
         <img src="https://picsum.photos/200" alt="" />
       </figure>
       <h2>{title}</h2>
-      <time>{date}</time>
+      <time>{date.toDateString()}</time>
       <section>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </section>
--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,19 +1,22 @@
 import Card from "./Card";
 const CardsList = () => {
   const localStorageData = [
-    { id: 1, title: "title A", date: "2024-06-25" },
-    { id: 2, title: "title B", date: "2024-06-24" },
-    { id: 3, title: "title C", date: "2024-06-23" },
-    { id: 4, title: "title Z", date: "2024-06-20" },
+    { id: 1, title: "title A", date: new Date("2019-06-25") },
+    { id: 2, title: "title B", date: new Date("2020-06-24") },
+    { id: 3, title: "title C", date: new Date("2024-06-23") },
+    { id: 4, title: "title D", date: new Date("2024-04-10") },
+    { id: 5, title: "title F", date: new Date("2024-05-23") },
+    { id: 6, title: "title Z", date: new Date("2024-06-26") },
   ];
 
-  console.log(localStorageData[0].id);
+  const SortedData = [...localStorageData].sort((d1, d2) => d2.date - d1.date);
+  // console.log(localStorageData[0].id);
 
-  localStorage.setItem("dairyCards", JSON.stringify(localStorageData));
+  // localStorage.setItem("dairyCards", JSON.stringify(localStorageData));
 
   return (
-    <main className="grid grid-cols-4 place-items-center p-8">
-      {localStorageData.map((card) => (
+    <main className="grid grid-cols-4 gap-4 place-items-center p-8 ">
+      {SortedData.map((card) => (
         <Card key={card.id} title={card.title} date={card.date} />
       ))}
     </main>
